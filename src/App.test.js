f6014ffb0f41b1a7967cf63d8353f79585b0579b
SaurabhK05 <saurabh.k150600@gmail.com>
2024-02-26 18:29:58 +0530

    added test case for app component

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,29 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
+import { Provider } from "react-redux";
+import ChartsDataStore from "./store/ChartsDataStore";
+import App from "./App";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe("App Component", () => {
+  test("renders title correctly", () => {
+    render(
+      <Provider store={ChartsDataStore}>
+        <App />
+      </Provider>
+    );
+    expect(screen.getByText("E-Commerce Dashboard")).toBeInTheDocument();
+  });
+
+  test("renders DashboardCards components", () => {
+    render(
+      <Provider store={ChartsDataStore}>
+        <App />
+      </Provider>
+    );
+    expect(screen.getByText(/Company Profit in \d{4}/i)).toBeInTheDocument();
+    expect(screen.getByText(/Star Product of \d{4} is/i)).toBeInTheDocument();
+    expect(screen.getByText(/Highest Users from/i)).toBeInTheDocument();
+    expect(screen.getByText(/Profit % in \d{4}/i)).toBeInTheDocument();
+  });
 });
